69598fef2ae8246172d247bad17c6ab997474c4f
s.tereshchenko <sergey.tereshenko@starcode.ru>
2016-08-15 06:33:41 -0500

    Fix main menu sticky

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,129 +1,132 @@
 $(function(){
+    $(document).ready( function () {
+        var mySwiper = new Swiper ('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            slidesPerView: 3,
+            paginationClickable: true,
+            spaceBetween: 5,
+            breakpoints: {
+                600: {
+                    slidesPerView: 1,
+                    spaceBetween: 10
+                },
+                1200: {
+                    slidesPerView: 2
+                }
+            },
+            // Navigation arrows
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
 
-    var mySwiper = new Swiper ('.swiper-container', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-      slidesPerView: 3,
-      paginationClickable: true,
-      spaceBetween: 5,
-	  breakpoints: {
-		  600: {
-			  slidesPerView: 1,
-			  spaceBetween: 10
-		  },
-		  1200: {
-			  slidesPerView: 2
-		  }
-	  },
-      // Navigation arrows
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
+        });
 
+
+        $('.js-show-search-dialog').click(function () {
+            showDialog({
+                text: '\
+                <form action="#" class="b-mobile-search-form">\
+                  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">\
+                    <input class="mdl-textfield__input" type="text" id="sample3">\
+                    <label class="mdl-textfield__label" for="sample3">Search</label>\
+                  </div>\
+                </form>\
+                ',
+                contentStyle: {'max-width': '100%'},
+                cancelable: true
+            });
+        });
+
+
+
+        /* !-jQuery extensions */
+
+        /* !- Check if element exists */
+        $.fn.exists = function() {
+            if ($(this).length > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        /* !- Check if element is loaded */
+        $.fn.loaded = function(callback, jointCallback, ensureCallback){
+            var len	= this.length;
+            if (len > 0) {
+                return this.each(function() {
+                    var	el		= this,
+                        $el		= $(el),
+                        blank	= "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+
+                    $el.on("load.dt", function(event) {
+                        $(this).off("load.dt");
+                        if (typeof callback == "function") {
+                            callback.call(this);
+                        }
+                        if (--len <= 0 && (typeof jointCallback == "function")){
+                            jointCallback.call(this);
+                        }
+                    });
+
+                    if (!el.complete || el.complete === undefined) {
+                        el.src = el.src;
+                    } else {
+                        $el.trigger("load.dt")
+                    }
+                });
+            } else if (ensureCallback) {
+                if (typeof jointCallback == "function") {
+                    jointCallback.call(this);
+                }
+                return this;
+            }
+        };
     });
+    /* jQuery extensions: end */
 
 
-    $('.js-show-search-dialog').click(function () {
-        showDialog({
-            text: '\
-			<form action="#" class="b-mobile-search-form">\
-			  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">\
-			    <input class="mdl-textfield__input" type="text" id="sample3">\
-			    <label class="mdl-textfield__label" for="sample3">Search</label>\
-			  </div>\
-			</form>\
-            ',
-            contentStyle: {'max-width': '100%'},
-            cancelable: true
+    /* Upload file */
+    $(document).ready( function () {
+
+        $(".b-tariff-requests__input-file").change(function () {
+            var str = $(this).val();
+            $(".b-tariff-requests__label-file").html(str);
+        });
+        $(".b-question__input-file").change(function () {
+            var str = $(this).val();
+            $(".b-question__label-file").html(str);
+        });
+
+        /* FAQ-accordeon*/
+
+        $(".c-faq-link").on("click", function (e) {
+            e.preventDefault();
+
+            var $this = $(this),
+                item = $this.closest(".b-faq-acc__item"),
+                list = $this.closest(".b-faq-acc__list"),
+                items = list.find(".b-faq-acc__item"),
+                content = $this.next(".b-faq-acc__item-content-wrap"),
+                all = list.find(".b-faq-acc__item-content-wrap");
+
+            if (!item.hasClass("active")) {
+
+                items.removeClass("active");
+                item.addClass("active");
+                all.stop(true, true).slideUp(300);
+                content.stop(true, true).slideDown(300);
+            } else {
+                content.stop(true, true).slideUp(300);
+                item.removeClass("active");
+            }
+            ;
         });
     });
 
 
-
-/* !-jQuery extensions */
-
-	/* !- Check if element exists */
-	$.fn.exists = function() {
-		if ($(this).length > 0) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/* !- Check if element is loaded */
-	$.fn.loaded = function(callback, jointCallback, ensureCallback){
-		var len	= this.length;
-		if (len > 0) {
-			return this.each(function() {
-				var	el		= this,
-					$el		= $(el),
-					blank	= "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
-
-				$el.on("load.dt", function(event) {
-					$(this).off("load.dt");
-					if (typeof callback == "function") {
-						callback.call(this);
-					}
-					if (--len <= 0 && (typeof jointCallback == "function")){
-						jointCallback.call(this);
-					}
-				});
-
-				if (!el.complete || el.complete === undefined) {
-					el.src = el.src;
-				} else {
-					$el.trigger("load.dt")
-				}
-			});
-		} else if (ensureCallback) {
-			if (typeof jointCallback == "function") {
-				jointCallback.call(this);
-			}
-			return this;
-		}
-	};
-
-/* jQuery extensions: end */
-
-
-	/* Upload file */
-
-	$(".b-tariff-requests__input-file").change(function(){
-		var str = $(this).val();
-		$(".b-tariff-requests__label-file").html(str);
-	});
-	$(".b-question__input-file").change(function(){
-		var str = $(this).val();
-		$(".b-question__label-file").html(str);
-	});
-
-	/* FAQ-accordeon*/
-
-	$(".c-faq-link").on("click", function(e){
-		e.preventDefault();
-
-		var $this = $(this),
-			item = $this.closest(".b-faq-acc__item"),
-			list = $this.closest(".b-faq-acc__list"),
-			items = list.find(".b-faq-acc__item"),
-			content = $this.next(".b-faq-acc__item-content-wrap"),
-			all = list.find(".b-faq-acc__item-content-wrap");
-
-		if(!item.hasClass("active")) {
-
-			items.removeClass("active");
-			item.addClass("active");
-			all.stop(true, true).slideUp(300);
-            content.stop(true, true).slideDown(300);
-		} else {
-            content.stop(true, true).slideUp(300);
-		    item.removeClass("active");
-        };
-	});
-
-
-	/* Become polar partner - animation */
+    /* Become polar partner - animation */
 
     $(document).ready( function () {
         var img = $(".b-vertex-background--partner"),
@@ -141,120 +144,86 @@ $(function(){
         header.delay(2000).animate({ opacity: 1}, 500);
         head.delay(2500).animate({ top: 0}, 2000);
         content.delay(4500).animate({ opacity: 1}, 500);
-	});
+    });
 
 
-	/* ShowPopup */
+    /* ShowPopup */
+    $(document).ready( function () {
+        $(".c-popup-show").on("click", function(e){
+            e.stopPropagation();
+            $(".b-popup").fadeIn(300);
+            $(".b-popup__content").fadeIn(300);
+            $(".b-popup__content").css("margin", "25px auto 0");
 
-	$(".c-popup-show").on("click", function(e){
-        e.stopPropagation();
-		$(".b-popup").fadeIn(300);
-		$(".b-popup__content").fadeIn(300);
-		$(".b-popup__content").css("margin", "25px auto 0");
+            $(".b-popup__content").click(function(e) {
+                e.stopPropagation();
+            });
+        });
 
-		$(".b-popup__content").click(function(e) {
-	       e.stopPropagation();
-		});
-	});
-
-    $("body").on("click", function(e){
-		$(".b-popup").fadeOut(300);
-		$(".b-popup__content").fadeOut(300);
-	});
-
-
-	/* Contact Us */
-
-	$(".b-contact__item").on("mouseenter", function(e){
-		var $this = $(this),
-			office = $this.children(".b-contact__office"),
-			items = office.children(".b-contact__office-item"),
-			head = office.children(".b-contact__office-head");
-
-		office.stop(true, true).slideDown(700);
-		items.stop(true, true).delay(700).fadeIn(1500);
-		head.stop(true, true).delay(700).fadeIn(1500);
-	});
-
-	$(".b-contact__item").on("mouseleave", function(e){
-		$(".b-contact__office").fadeOut(500);
-	});
+        $("body").on("click", function(e){
+            $(".b-popup").fadeOut(300);
+            $(".b-popup__content").fadeOut(300);
+        });
+    });
 
 
-  /* Static main menu */
+    /* Contact Us */
+    $(document).ready( function () {
+        $(".b-contact__item").on("mouseenter", function (e) {
+            var $this = $(this),
+                office = $this.children(".b-contact__office"),
+                items = office.children(".b-contact__office-item"),
+                head = office.children(".b-contact__office-head");
+
+            office.stop(true, true).slideDown(700);
+            items.stop(true, true).delay(700).fadeIn(1500);
+            head.stop(true, true).delay(700).fadeIn(1500);
+        });
+
+        $(".b-contact__item").on("mouseleave", function (e) {
+            $(".b-contact__office").fadeOut(500);
+        });
+    });
+
+
+    /* Static main menu */
 
     $(document).ready( function () {
-      var topScroll,
-          topRowHeight = $('.b-top-row').height();
+        var topScroll,
+            topRowHeight = $('.b-top-row').height();
 
-      $('body > .mdl-js-layout').on('scroll', function(){
+        $(window).on('scroll', function(){
 
-        topScroll = $(this).scrollTop();
+            topScroll = $(this).scrollTop();
 
-        if(topScroll > topRowHeight) {
-          $('body').addClass('js-scrolling');
-          $('.js-scrolling header').css({'right' : getScrollBarWidth() + 'px', 'padding-left' : '17px'});
-          console.log('1');
-        } else {
+            if(topScroll > topRowHeight) {
+                $('body').addClass('js-scrolling');
+            } else {
+                $('body').removeClass('js-scrolling');
+            }
 
-            $('.js-scrolling header').css({'right' : 'auto', 'padding-left' : '0px'});
-            $('body').removeClass('js-scrolling');
-
-        }
-
-      });
-
-      function getScrollBarWidth() {
-        var inner = document.createElement('p');
-        inner.style.width = "100%";
-        inner.style.height = "200px";
-
-        var outer = document.createElement('div');
-        outer.style.position = "absolute";
-        outer.style.top = "0px";
-        outer.style.left = "0px";
-        outer.style.visibility = "hidden";
-        outer.style.width = "200px";
-        outer.style.height = "150px";
-        outer.style.overflow = "hidden";
-        outer.appendChild (inner);
-
-        document.body.appendChild (outer);
-        var w1 = inner.offsetWidth;
-        outer.style.overflow = 'scroll';
-        var w2 = inner.offsetWidth;
-        if (w1 == w2) w2 = outer.clientWidth;
-
-        document.body.removeChild (outer);
-
-        return (w1 - w2);
-      };
-
-  // $('.js-main-menu-static').find('.b-top-row').hide();
-  // $('.js-main-menu-static').css({
-  //   'position' : 'fixed',
-  //   'top' : 0,
-  //   'left' : 0
-  // });
+        });
 
     });
-	/* News-form */
 
-	$(document).ready( function () {
-		var input1 = $(".c-period-yes"),
-		    input2 = $(".c-period-no"),
-			select = $(".b-news-filters__period-select");
+
+    /* News-form */
+    $(document).ready( function () {
+        var input1 = $(".c-period-yes"),
+            input2 = $(".c-period-no"),
+            select = $(".b-news-filters__period-select");
 
         $(input1).click(function(e) {
-           select.prop('disabled', false);
+            select.prop('disabled', false);
         });
         $(input2).click(function(e) {
-           select.prop('disabled', true);
+            select.prop('disabled', true);
         });
-	});
+    });
 
 
     /* !-Royal Slider */
+    $(document).ready( function () {
     if ($(".rsHomePorthole").exists()) {
         var portholeSlider = {};
         portholeSlider.container = $("#main-slideshow");
@@ -301,7 +270,7 @@ $(function(){
             }
         }).data("royalSlider");
         var $_this = portholeSlider.container,
-            $_this_childs = $_this.find(".rsSlide").size();
+            $_this_childs = $_this.find(".rsSlide").length;
         if ($_this_childs < 2) {
             $(".rsThumbs", $_this).hide();
             portholeSlider.api._isMove = false;
@@ -341,7 +310,7 @@ $(function(){
             }).data("royalSlider");
         });
     }, true);
-
+    });
     /* Royal Slider: end */
 
 });
